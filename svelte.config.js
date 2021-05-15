57ec836a735a2f169efc99caefe1b6a4674edeb6
rirurin <rirurin@rirurin.com>
2021-05-15 18:46:20 +0900

    go

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,8 @@
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+import vercel from '@sveltejs/adapter-vercel';
+
+export default {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: vercel()
 	}
 };
-
-export default config;
